397d00b7b0c71dd15082c84e4082bfb20bebc3ec
Shiva9949 <darashanojushivachary@gmail.com>
2022-04-30 17:12:16 +0530

    update files

---
+++ src/main/java/com/customers/customermgmt/dto/CustomerNameAndCity.java
@@ -0,0 +1,15 @@
+package com.customers.customermgmt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerNameAndCity {
+	
+	String name;
+	String city;
+
+}
